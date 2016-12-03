0a9fc21dd4fbf8eb746bf3ad1aafbd65c7be9f50
venkat24 <venkatramansrikanth@outlook.com>
2016-12-04 00:32:37 +0530

    Basic Parsing

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,4 +1,35 @@
 module Main where
 
+import Data.Maybe
+
+data Token = Increment
+           | Decrement
+           | MoveLeft
+           | MoveRight
+           | Output
+           | Input
+           | LoopL
+           | LoopR
+           | Comment Char
+           deriving (Show)
+
+type Program = [Token]
+
+parse :: String -> Program
+parse src = mapMaybe mapping src
+    where
+        mapping c = case c of
+            '-' -> Just Decrement
+            '+' -> Just Increment
+            '<' -> Just MoveLeft
+            '>' -> Just MoveRight
+            '.' -> Just Output
+            ',' -> Just Input
+            '[' -> Just LoopL
+            ']' -> Just LoopR
+            x  -> Nothing
+
 main = do
-    putStrLn "Hello World"
+    programSrc <- getLine
+    print $ parse programSrc
+
